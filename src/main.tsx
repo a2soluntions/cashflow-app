4e41fb08c8df2327ee398b6854a520e51c207080
Metalize Previato <metalizzepreviato@gmail.com>
2026-01-25 04:39:06 -0300

    Fix: Create redirects file correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // <--- ESSA LINHA É O SEGREDO! SEM ELA, A BARRA NÃO MUDA.
+import './index.css'; // <--- ESSA LINHA É OBRIGATÓRIA
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
